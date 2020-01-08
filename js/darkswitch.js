ce570150d6d4b9c3da9df16d0e2d760e4b636e59
robibrutoczki <52542134+robibrutoczki@users.noreply.github.com>
2020-01-08 16:20:50 +0000

    UPDATE

---
+++ js/darkswitch.js
@@ -0,0 +1,8 @@
+$("#darkSwitch").click(function (b) {
+    b.preventDefault();
+    $("tbody,.btn,.nav-link,.sidebar-heading,.btn-light,.modal-header,.modal-footer,.modal-body,.d-flex,#body,.sidebar-wrapper,.jumbotron,.navbar,a,.card,body").toggleClass("black");
+
+
+});
+
+console.log("hello");
