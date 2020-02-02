209ea124ec297513781d21abdce344ec20816f00
robibrutoczki <52542134+robibrutoczki@users.noreply.github.com>
2020-02-02 18:16:19 +0000

    redesigned colors,menu,added contact

--- js/darkswitch.js
+++ js/darkswitch.js
@@ -1,6 +1,8 @@
 $("#darkSwitch").click(function (b) {
     b.preventDefault();
-    $("tbody,.btn,.nav-link,.sidebar-heading,.btn-light,.modal-header,.modal-footer,.modal-body,.d-flex,#body,.sidebar-wrapper,.jumbotron,.navbar,a,.card,body").toggleClass("black");
+    $("marquee,.card-deck,.jumbotron,.container-fluid,tbody,.btn,.nav-link,.sidebar-heading,.btn-light,.modal-header,.modal-footer,.modal-body,.d-flex,#body,.sidebar-wrapper,.jumbotron,.navbar,a,.card,body").toggleClass("black");
+    $(".card-ch").toggleClass("dark");
+
 
 
 });
