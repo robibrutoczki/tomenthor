28a0f0b3dd39e17bd3ff5283d7d584473fa0cef6
robibrutoczki <52542134+robibrutoczki@users.noreply.github.com>
2020-02-04 19:16:11 +0000

    CSS marquee added to exchange

--- js/darkswitch.js
+++ js/darkswitch.js
@@ -1,6 +1,6 @@
 $("#darkSwitch").click(function (b) {
     b.preventDefault();
-    $("marquee,.card-deck,.jumbotron,.container-fluid,tbody,.btn,.nav-link,.sidebar-heading,.btn-light,.modal-header,.modal-footer,.modal-body,.d-flex,#body,.sidebar-wrapper,.jumbotron,.navbar,a,.card,body").toggleClass("black");
+    $(".marquee,.card-deck,.jumbotron,.container-fluid,tbody,.btn,.nav-link,.sidebar-heading,.btn-light,.modal-header,.modal-footer,.modal-body,.d-flex,#body,.sidebar-wrapper,.jumbotron,.navbar,a,.card,body").toggleClass("black");
     $(".card-ch").toggleClass("dark");
 
 
